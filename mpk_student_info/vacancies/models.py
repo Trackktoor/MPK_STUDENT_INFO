e35743d6c755cf5ee02f520a92d9323c162bc8ae
Trackktoor <pipleson212@gmail.com>
2023-05-06 22:57:11 +0500

    fix models

--- mpk_student_info/vacancies/models.py
+++ mpk_student_info/vacancies/models.py
@@ -1,30 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 
 class Student(models.Model):
-    LastName = models.CharField(150)
-    Name = models.CharField(150)
-    FatherName = models.CharField(150)
+    LastName = models.CharField(verbose_name='Фамилия', max_length=150)
+    Name = models.CharField(max_length=150, verbose_name='Имя')
+    FatherName = models.CharField(max_length=150, verbose_name='Отчество')
 
-    Group = models.CharField(200)
-    PhoneNumber = models.CharField(60)
-    EndCollege = models.DateField()
-    Speciality = models.CharField(200)
-    Employment = models.CharField(500)
+    Group = models.CharField(max_length=200, verbose_name='Группа')
+    PhoneNumber = models.CharField(max_length=60, verbose_name='Номер телефона')
+    EndCollege = models.DateField(verbose_name='Окончание колледжа')
+    Speciality = models.CharField(max_length=200, verbose_name='Специальность')
+    Employment = models.CharField(max_length=500, verbose_name='Трудоустройство')
     
-    User = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    User = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True, verbose_name='Пользователь')
 
 class Company(models.Model):
-    CompanyName = models.CharField(200)
-    PhoneNumber = models.CharField(60)
+    CompanyName = models.CharField(max_length=200, verbose_name='Название компании')
+    PhoneNumber = models.CharField(max_length=60, verbose_name='Номер телефона')
 
 class Speciality(models.Model):
-    SpecialityName = models.CharField(300)
+    SpecialityName = models.CharField(max_length=300, verbose_name='Название специальности')
 
 class Vacancy(models.Model):
-    CreatedDate = models.DateTimeField(auto_now=True)
-    Salary = models.IntegerField(blank=True, null=True)
+    CreatedDate = models.DateTimeField(auto_now=True, verbose_name='Дата оздания')
+    Salary = models.IntegerField(blank=True, null=True, verbose_name='Зарплата')
 
-    Speciality = models.ForeignKey(Speciality, on_delete = models.CASCADE)
-    Company = models.ForeignKey(Company, on_delete = models.CASCADE)
+    Speciality = models.ForeignKey(Speciality, on_delete = models.CASCADE, verbose_name='Специальность')
+    Company = models.ForeignKey(Company, on_delete = models.CASCADE, verbose_name='Компания')
