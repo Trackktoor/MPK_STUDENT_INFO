cfe363fdcc49b5b89dd4f877fb5cbea6cdc287dd
Trackktoor <pipleson212@gmail.com>
2023-05-06 22:45:12 +0500

    Registrate models in admin-panel

--- mpk_student_info/vacancies/admin.py
+++ mpk_student_info/vacancies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student,Company,Speciality,Vacancy
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Company)
+admin.site.register(Speciality)
+admin.site.register(Vacancy)
