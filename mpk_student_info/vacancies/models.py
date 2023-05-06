f842484079404f4d21896cfc95dada5c8f7309e9
Trackktoor <pipleson212@gmail.com>
2023-05-06 23:07:03 +0500

    update models, add str method

--- mpk_student_info/vacancies/models.py
+++ mpk_student_info/vacancies/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib import admin
 
 
 class Student(models.Model):
@@ -16,16 +15,28 @@ class Student(models.Model):
     
     User = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True, verbose_name='Пользователь')
 
+    def __str__(self):
+        return (f'{self.LastName} {self.Name} {self.FatherName}')
+
 class Company(models.Model):
     CompanyName = models.CharField(max_length=200, verbose_name='Название компании')
     PhoneNumber = models.CharField(max_length=60, verbose_name='Номер телефона')
 
+    def __str__(self):
+        return (f'{self.CompanyName}')
+       
 class Speciality(models.Model):
     SpecialityName = models.CharField(max_length=300, verbose_name='Название специальности')
-
+    
+    def __str__(self):
+        return (f'{self.SpecialityName}')
+    
 class Vacancy(models.Model):
     CreatedDate = models.DateTimeField(auto_now=True, verbose_name='Дата оздания')
     Salary = models.IntegerField(blank=True, null=True, verbose_name='Зарплата')
 
     Speciality = models.ForeignKey(Speciality, on_delete = models.CASCADE, verbose_name='Специальность')
     Company = models.ForeignKey(Company, on_delete = models.CASCADE, verbose_name='Компания')
+
+    def __str__(self):
+        return (f'{self.Speciality.SpecialityName}')
