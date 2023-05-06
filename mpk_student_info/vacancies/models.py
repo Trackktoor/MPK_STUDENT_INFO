73a7ad3f36575b763fb1a5ad17d7905eeae71c80
Trackktoor <pipleson212@gmail.com>
2023-05-06 22:41:55 +0500

    Add models for Vacancies

--- mpk_student_info/vacancies/models.py
+++ mpk_student_info/vacancies/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Student(models.Model):
+    LastName = models.CharField(150)
+    Name = models.CharField(150)
+    FatherName = models.CharField(150)
+
+    Group = models.CharField(200)
+    PhoneNumber = models.CharField(60)
+    EndCollege = models.DateField()
+    Speciality = models.CharField(200)
+    Employment = models.CharField(500)
+    
+    User = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+
+class Company(models.Model):
+    CompanyName = models.CharField(200)
+    PhoneNumber = models.CharField(60)
+
+class Speciality(models.Model):
+    SpecialityName = models.CharField(300)
+
+class Vacancy(models.Model):
+    CreatedDate = models.DateTimeField(auto_now=True)
+    Salary = models.IntegerField(blank=True, null=True)
+
+    Speciality = models.ForeignKey(Speciality, on_delete = models.CASCADE)
+    Company = models.ForeignKey(Company, on_delete = models.CASCADE)
